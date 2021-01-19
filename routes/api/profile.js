261f37efadc8fdfeef0742052891af0662c327d7
Chris Rico <chris.r.rico@gmail.com>
2021-01-19 13:31:00 -0800

    added Get route to get all profiles

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -104,7 +104,23 @@ async (req,res) => {
         console.error(err.message);
         res.status(500).send('Server Error');
         }
-});
+    }
+);
+
+// @route   GET api/profile
+// @desc    Get all profiles
+// @access  Public
+
+router.get('/', async(req,res) => {
+    try {
+        const profiles = await Profile.find().populate('user', ['name', 'avatar']);
+        res.json(profiles);
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server Error');
+    }
+})
+
 
 
 module.exports = router;
