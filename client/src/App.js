e9bd3972855f0ab2d43383d0d4f83798bd24c8ed
Chris Rico <chris.r.rico@gmail.com>
2021-01-21 15:24:09 -0800

    wrapped the App in a Router and imported routes

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,15 @@ import './App.css';
 import React, { Fragment } from 'react';
 import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 const App = () => (
+  <Router>
     <Fragment>
       <Navbar />
-      <Landing />
+      <Route exact path='/' component={Landing} />
     </Fragment>
+  </Router>
 );
 
 export default App;
