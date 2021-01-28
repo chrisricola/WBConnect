4702c282073aa18bf331ee61bb34a7b3bfa825a3
Chris Rico <chris.r.rico@gmail.com>
2021-01-27 17:55:26 -0800

    added education file, route and profile

--- client/src/App.js
+++ client/src/App.js
@@ -15,6 +15,7 @@ import PrivateRoute from './components/routing/PrivateRoute';
 import CreateProfile from './components/profile-forms/CreateProfile';
 import EditProfile from './components/profile-forms/EditProfile';
 import AddExperience from './components/profile-forms/AddExperience';
+import AddEducation from './components/profile-forms/AddEducation';
 
 if (localStorage.token) {
   setAuthToken(localStorage.token);
@@ -39,6 +40,7 @@ const App = () => {
           <PrivateRoute exact path="/create-profile" component={CreateProfile} />
           <PrivateRoute exact path="/edit-profile" component={EditProfile} />
           <PrivateRoute exact path="/add-experience" component={AddExperience} />
+          <PrivateRoute exact path="/add-education" component={AddEducation} />
         </Switch>
       </section>
     </Fragment>
