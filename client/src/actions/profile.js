32f7757355b01c27f97099079a7494a65ebee78f
Chris Rico <chris.r.rico@gmail.com>
2021-01-29 09:48:30 -0800

    finished Profile actions and reducers with get profile by Id, get all profiles and get repos

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -2,10 +2,12 @@ import axios from 'axios';
 import { setAlert } from './alert';
 import {
     GET_PROFILE,
+    GET_PROFILES,
     PROFILE_ERROR,
     UPDATE_PROFILE,
     CLEAR_PROFILE,
-    ACCOUNT_DELETED
+    ACCOUNT_DELETED,
+    GET_REPOS
 } from './types';
 
 // Get current users profile 
@@ -16,6 +18,61 @@ export const getCurrentProfile = () => async dispatch => {
 
         dispatch ({
             type: GET_PROFILE,
+            payload: res.data
+        });
+    } catch (err) {
+        dispatch ({
+            type: PROFILE_ERROR,
+            payload: { msg: err.response.statusTex, status: err.response.status }
+        });
+    }
+};
+
+// Get all profiles
+
+export const getProfiles = () => async dispatch => {
+    dispatch({ type: CLEAR_PROFILE });
+    try {
+        const res = await axios.get('api/profile');
+
+        dispatch ({
+            type: GET_PROFILES,
+            payload: res.data
+        });
+    } catch (err) {
+        dispatch ({
+            type: PROFILE_ERROR,
+            payload: { msg: err.response.statusTex, status: err.response.status }
+        });
+    }
+};
+
+// Get profile by ID
+
+export const getProfileById = userId => async dispatch => {
+    try {
+        const res = await axios.get(`api/profile/user/${userId}`);
+
+        dispatch ({
+            type: GET_PROFILE,
+            payload: res.data
+        });
+    } catch (err) {
+        dispatch ({
+            type: PROFILE_ERROR,
+            payload: { msg: err.response.statusTex, status: err.response.status }
+        });
+    }
+};
+
+// Get Github repos
+
+export const getGithubRepos = username => async dispatch => {
+    try {
+        const res = await axios.get(`/api/profile/github/${username}`);
+
+        dispatch ({
+            type: GET_REPOS,
             payload: res.data
         });
     } catch (err) {
@@ -173,7 +230,7 @@ export const deleteAccount = () => async dispatch => {
     
             dispatch ({ type: CLEAR_PROFILE });
             dispatch ({ type: ACCOUNT_DELETED });
-            
+
             dispatch(setAlert('Your account has been deleted'));
         } catch (err) {
             dispatch ({
