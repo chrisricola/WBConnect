a3529bbf8c3db6aed98ef93099df55e9b82d1335
Chris Rico <chris.r.rico@gmail.com>
2021-02-01 12:38:52 -0800

    bringing in profiles link to user profiles

--- client/src/components/profiles/Profiles.js
+++ client/src/components/profiles/Profiles.js
@@ -2,6 +2,7 @@ import React, { Fragment, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import Spinner from '../layout/Spinner';
+import ProfileItem from './ProfileItem';
 import { getProfiles } from '../../actions/profile';
 
 
@@ -16,6 +17,13 @@ const Profiles = ({ getProfiles, profile: { profiles, loading }}) => {
             <p className="lead">
                 <i className="fab fa-connectdevelop"></i>Browse and conncet with developers
             </p>
+            <div className="profiles">
+                {profiles.length > 0 ? (
+                    profiles.map(profile => (
+                        <ProfileItem key={profile._id} profile={profile} />
+                    ))
+                ) : <h4>No profile found...</h4>}
+            </div>
             </Fragment> 
         }
     </Fragment>
