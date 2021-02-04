cc883dc0a4ad89f6ba1568b9d3f64eb84fd211b0
Chris Rico <chris.r.rico@gmail.com>
2021-02-03 22:16:32 -0800

    added skills to user profile

--- client/src/components/profile/ProfileAbout.js
+++ client/src/components/profile/ProfileAbout.js
@@ -9,7 +9,7 @@ const ProfileAbout = ({ profile: {
     <div class="profile-about bg-light p-2">
     {bio && (
         <Fragment>
-         <h2 class="text-primary">{name}s Bio</h2>
+         <h2 class="text-primary">{name.trim().split(' ')[0]}s Bio</h2>
         <p>
             {bio}
         </p>
@@ -20,11 +20,11 @@ const ProfileAbout = ({ profile: {
           
           <h2 class="text-primary">Skill Set</h2>
           <div class="skills">
-            <div class="p-1"><i class="fa fa-check"></i> HTML</div>
-            <div class="p-1"><i class="fa fa-check"></i> CSS</div>
-            <div class="p-1"><i class="fa fa-check"></i> JavaScript</div>
-            <div class="p-1"><i class="fa fa-check"></i> Python</div>
-            <div class="p-1"><i class="fa fa-check"></i> C#</div>
+              {skills.map((skill,index) => (
+                  <div key={index} className="p-1">
+                    <i className="fas fa-check"></i> {skill}
+                    </div>
+              ))}
           </div>
         </div>;
     
