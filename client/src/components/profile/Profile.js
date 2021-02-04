d3ded804427e3fc202bf6e52a110359eacdfc48c
Chris Rico <chris.r.rico@gmail.com>
2021-02-03 23:23:15 -0800

    added profile education

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -7,6 +7,7 @@ import { Link } from 'react-router-dom';
 import ProfileTop from './ProfileTop';
 import ProfileAbout from './ProfileAbout';
 import ProfileExperience from './ProfileExperience.js';
+import ProfileEducation from './ProfileEducation.js';
 
 const Profile = ({ getProfileById, profile: { profile, loading }, auth, match }) => {
     useEffect(() => {
@@ -33,6 +34,17 @@ const Profile = ({ getProfileById, profile: { profile, loading }, auth, match })
                         />
                       ))}
                     </Fragment>) : (<h4>No experience credentials</h4>)}
+                  </div>
+                  <div className="profile-edu bg-white p-2">
+                    <h2 className="textprimary">Education</h2>
+                    {profile.education.length > 0 ? (<Fragment>
+                      {profile.education.map(education => (
+                        <ProfileEducation
+                        key={education._id} 
+                        education={education} 
+                        />
+                      ))}
+                    </Fragment>) : (<h4>No education credentials</h4>)}
                   </div>
                 </ div>
                 </Fragment>}
